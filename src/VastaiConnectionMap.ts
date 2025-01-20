0a7b5cfbea0b694d46febc42829bf5e53fa9a01a
Matt Taylor <matt@matthewjosephtaylor.com>
2025-01-20 09:12:37 -0600

    initial-commit

---
+++ src/VastaiConnectionMap.ts
@@ -0,0 +1,21 @@
+
+export type VastaiConnectionMap = {
+  "vastai.search": {
+    request: {
+      options?: Partial<{}>;
+      body: {
+        area?: "offers";
+        query: string;
+      };
+    };
+    response: {
+      delta?: string;
+      text?: string;
+      done?: boolean;
+    };
+    headers: {
+      url?: string;
+      authToken?: string;
+    };
+  };
+};
